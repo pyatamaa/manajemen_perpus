3344a837410f5c6f226693a6524306d60ae692db
Nur Pratama <92738041+pyatamaa@users.noreply.github.com>
2024-07-10 19:09:04 +0700

    perubahan active pada sidebar

--- manajemen_perpustakaan/includes/sidebar.php
+++ manajemen_perpustakaan/includes/sidebar.php
@@ -1,26 +1,32 @@
+<?php
+// Mendapatkan nama file saat ini untuk menentukan menu yang aktif
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse fixed-top">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
+            <img src="img/logo_al_muttaqin.png" alt="Logo" style="max-width: 150px;">
             <li class="nav-item">
-                <a class="nav-link active" href="dashboard.php">
+                <a class="nav-link <?php echo $current_page == 'dashboard.php' ? 'active' : ''; ?>" href="dashboard.php">
                     <span data-feather="home"></span>
                     Dashboard
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="buku.php">
+                <a class="nav-link <?php echo $current_page == 'buku.php' ? 'active' : ''; ?>" href="buku.php">
                     <span data-feather="book"></span>
                     Data Buku
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="peminjaman.php">
+                <a class="nav-link <?php echo $current_page == 'peminjaman.php' ? 'active' : ''; ?>" href="peminjaman.php">
                     <span data-feather="file"></span>
                     Data Peminjaman
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="pengembalian.php">
+                <a class="nav-link <?php echo $current_page == 'pengembalian.php' ? 'active' : ''; ?>" href="pengembalian.php">
                     <span data-feather="file"></span>
                     Data Pengembalian
                 </a>
@@ -28,3 +34,10 @@
         </ul>
     </div>
 </nav>
+
+<style>
+.nav-link.active {
+    background-color: #343a40; /* Warna abu-abu gelap */
+    color: #fff;
+}
+</style>
